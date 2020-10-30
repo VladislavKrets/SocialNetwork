9b9892a365ff8fec8d3ee6181790bf35ff3eaf82
Vladislav Krets <vladislav.krets@gmail.com>
2020-10-30 21:25:25 +0300

    fixed group post permission

--- core/permissions.py
+++ core/permissions.py
@@ -60,6 +60,7 @@ class GroupPostPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-
+        if request.method in ('PATCH', 'PUT'):
+            return obj.user == request.user
         return obj.user == request.user or obj.group.creator == request.user
 

70018ff64eb917ab3546ebb0fecb547d5002cacb
Vladislav Krets <vladislav.krets@gmail.com>
2020-12-13 12:58:20 +0300

    exit alert

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import './Navbar.css'
 import user from '../../img/user.svg'
 import exit from '../../img/exit.svg'
 import {useHistory} from 'react-router-dom';
+import Alert from "../Alert/Alert";
 
 function NavBar(props) {
     let history = useHistory();
+
+    const [isExitDialogOpened, setExit] = useState(false)
 
     const profileRedirect = () => {
         let path = `/me`;
@@ -19,6 +22,55 @@ function NavBar(props) {
         color: '#3e7cb0'
     }
     return <div>
+        {
+            isExitDialogOpened ?
+                <Alert style={{backgroundColor: '#f7faff', borderRadius: '12px',}}
+                       close={() => {
+                           setExit(false)
+                       }}>
+                    <div style={{
+                        width: '300px',
+                        borderRadius: '12px',
+                        backgroundColor: '#f7faff'
+                    }}>
+                        <div style={{padding: '20px 12px', wordBreak: 'break-word', textAlign: 'center'}}>
+                            Вы действительно хотите выйти из этого аккаунта?
+                        </div>
+                        <div style={{
+                            display: 'flex',
+                            fontWeight: 'bold',
+                            width: '100%',
+                            borderTop: '1px solid grey'
+                        }}>
+                            <div style={{
+                                textAlign: 'center',
+                                width: '50%',
+                                borderRight: '1px solid grey',
+                                padding: '12px 0',
+                                color: 'red',
+                                cursor: 'pointer'
+                            }} onClick={() => {
+                                props.setCurrentLink({current: null})
+                                props.logOut()
+                            }}>
+                                Выйти
+                            </div>
+                            <div style={{
+                                width: '50%',
+                                textAlign: 'center',
+                                padding: '12px 0',
+                                color: '#3e7cb0',
+                                cursor: 'pointer'
+                            }} onClick={() => {
+                                setExit(false)
+                            }}>
+                                Отмена
+                            </div>
+                        </div>
+                    </div>
+                </Alert>
+                : null
+        }
         <div className='navbar-desktop'>
             <ul className='ul-navbar-desktop'>
                 {
@@ -45,8 +97,7 @@ function NavBar(props) {
                 <div style={{paddingLeft: '7px'}}>
                     <a className={'exit-nav-bar'}
                        style={{cursor: 'pointer'}} onClick={() => {
-                        props.setCurrentLink({current: null})
-                        props.logOut()
+                        setExit(true)
                     }}>Выйти</a>
                 </div>
             </div>
@@ -54,24 +105,23 @@ function NavBar(props) {
         <div className='navbar-mobile'>
             <div>{document.title}</div>
             <div onClick={() => {
-                        props.setCurrentLink({current: null})
-                        props.logOut()
-                    }}><img style={{width: '28px', 'height': '28px'}} src={exit}/></div>
+                setExit(true)
+            }}><img style={{width: '28px', 'height': '28px'}} src={exit}/></div>
         </div>
         <div className='navbar-content'>
             {props.children}
         </div>
         <div className='epicbar-mobile'>
             <div onClick={profileRedirect} style={{
-                    padding: '0 10px',
-                    borderBottom: window.location.pathname === '/me' ?  '2px solid #3e7cb0' : null
-                }}>
+                padding: '0 10px',
+                borderBottom: window.location.pathname === '/me' ? '2px solid #3e7cb0' : null
+            }}>
                 <img style={{width: '28px', 'height': '28px'}} src={user}/>
             </div>
             {props.links.map(link => {
                 return <Link to={link.link} style={{
                     padding: '0 10px',
-                    borderBottom: window.location.pathname === link.link ?  '2px solid #3e7cb0' : null
+                    borderBottom: window.location.pathname === link.link ? '2px solid #3e7cb0' : null
                 }}
                              onClick={() => {
                                  props.setCurrentLink(link.text)
